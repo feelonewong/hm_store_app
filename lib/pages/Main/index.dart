52eb7b3687d1fc4708a5abb6f60590c37537ae90
MilesEvan123 <jackbruno19870807@gmail.com>
2026-02-17 21:14:28 +0800

    feat: 完成底部导航

--- lib/pages/Main/index.dart
+++ lib/pages/Main/index.dart
@@ -10,6 +10,38 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final List<Map<String, String>> _tabList = [
+      {
+        "icon": "lib/assets/home_normal.png",
+        "active_icon": "lib/assets/home_active.png",
+        "text": "首页",
+      },
+      {
+        "icon": "lib/assets/pro.png",
+        "active_icon": "lib/assets/pro_active.png",
+        "text": "分类",
+      },
+      {
+        "icon": "lib/assets/cart.png",
+        "active_icon": "lib/assets/cart_active.png",
+        "text": "购物车",
+      },
+      {
+        "icon": "lib/assets/my.png",
+        "active_icon": "lib/assets/my_active.png",
+        "text": "我的",
+      },
+    ];
+    List<BottomNavigationBarItem> items = _tabList.map((item) {
+      return BottomNavigationBarItem(
+        icon: Image.asset(item['icon']!, width: 24, height: 24),
+        activeIcon: Image.asset(item['active_icon']!, width: 24, height: 24),
+        label: item['text'],
+      );
+    }).toList();
+    return Scaffold(
+      body: Center(child: Text('Main Page')),
+      bottomNavigationBar: BottomNavigationBar(items: items),
+    );
   }
 }
